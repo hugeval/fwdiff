5cb3300977f44d0505c20ed01d4b2b6cc6f4e82c
vvelykyi <valhuge@gmail.com>
2020-04-13 18:58:36 +0300

    - Fixed laravel LoginController for oauth

--- src/Laravel/app/Http/Controllers/LoginController.php
+++ src/Laravel/app/Http/Controllers/LoginController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
+    use \Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Configures middleware
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
     /**
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
@@ -21,16 +40,30 @@ class LoginController extends Controller
     }
 
     /**
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function handleProviderCallback()
     {
         $user = Socialite::driver('google')->user();
-        $authUser = new User();
-        $authUser->setAttribute('name', $user->getName());
-        $authUser->setAttribute('email', $user->getEmail());
-        Auth::setUser($authUser);
+        $email = $user->getEmail();
+        $authUser = (new User())->newQuery()
+            ->where('email', $email)
+            ->first();
+        if (!$authUser) {
+            $authUser = new User();
+        }
+        $authUser->setAttribute('email', $email);
+        $authUser->setAttribute('google_id', $user->getId());
+        if (!$authUser->getAttribute('name')) {
+            $authUser->setAttribute('name', $user->getName());
+        }
+        if (!$authUser->getAuthPassword()) {
+            $authUser->setAttribute('password', Hash::make(microtime(true)));
+        }
+        $authUser->save();
+        Auth::guard()->login($authUser);
 
-        return redirect('/');
+        return $this->sendLoginResponse(app('request'));
     }
 }
